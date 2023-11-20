87522a95865e7a43c638c738d10c82a7de574443
unknown <nvyas3095@gmail.com>
2023-11-19 17:04:39 -0800

    Implemented searching products by pressing the category links within the side bar. Defined routes and configured the Router based on those routes. Defined the Router Outlet and set up Router Links to pass the category id parameter. The ProductListComponent will now read a category id parameter. Modified the Spring Boot app by creating a new method within the REST Repository. Updated the Angular Service to call the new URL on the Spring Boot app.

--- frontend/nononiki-ecommerce/src/app/components/product-list/product-list.component.ts
+++ frontend/nononiki-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -1,26 +1,43 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { Product } from 'src/app/common/product';
 import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list-grid.component.html',
-  // templateUrl: './product-list-table.component.html',
-  // templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent implements OnInit {
   
   products: Product[] = [];
+  currentCategoryId: number = 1;
 
-  constructor(private productService: ProductService) {}
+  constructor(private productService: ProductService,
+              private route: ActivatedRoute) {}
   
   ngOnInit(): void {
-    this.listProducts();  
+    this.route.paramMap.subscribe(() => {
+      this.listProducts();
+    });
   }
   
   listProducts() {
-    this.productService.getProductList().subscribe(
+    // check if "id" parameter is available
+    const hasCategoryId: boolean = this.route.snapshot.paramMap.has('id');
+
+    if (hasCategoryId) {
+      // get the 'id' param string, convert string into number using "+" symbol
+      // '!' is used to tell the compiler that we are expecting a non-null value
+      this.currentCategoryId = +this.route.snapshot.paramMap.get('id')!;
+    } 
+    else {
+      // default to category id of 1
+      this.currentCategoryId = 1;
+    }
+
+    // get products for given category id
+    this.productService.getProductList(this.currentCategoryId).subscribe(
       data => {
         this.products = data;
       }
