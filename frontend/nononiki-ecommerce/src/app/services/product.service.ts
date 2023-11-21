a63f56b11818b5d5c0d52af730053d8cf48ed33c
unknown <nvyas3095@gmail.com>
2023-11-20 16:48:26 -0800

    Created a Product Category class and a Component for the Product Category Menu. Enhanced Product Category Menu to read data from productService and updated productService to call product category URL from the Spring Boot app. Replaced hard-coded links with Menu components.

--- frontend/nononiki-ecommerce/src/app/services/product.service.ts
+++ frontend/nononiki-ecommerce/src/app/services/product.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Product } from '../common/product';
 import { Observable, map } from 'rxjs';
+import { ProductCategory } from '../common/product-category';
 
 @Injectable({
   providedIn: 'root'
@@ -9,20 +10,33 @@ import { Observable, map } from 'rxjs';
 export class ProductService {
 
   private baseUrl = 'http://localhost:8080/api/products';
+  private categoryUrl = 'http://localhost:8080/api/product-category';
 
   constructor(private httpClient: HttpClient) { }
 
   getProductList(theCategoryId: number): Observable<Product[]> {
     const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`;
     
-    return this.httpClient.get<GetResponse>(searchUrl).pipe(
+    return this.httpClient.get<GetResponseProducts>(searchUrl).pipe(
       map(response => response._embedded.products)
+    );
+  }
+
+  getProductCategories(): Observable<ProductCategory[]> {
+    return this.httpClient.get<GetResponseProductCategory>(this.categoryUrl).pipe(
+      map(response => response._embedded.productCategory)
     );
   }
 }
 
-interface GetResponse {
+interface GetResponseProducts {
   _embedded: {
     products: Product[];
+  }
+}
+
+interface GetResponseProductCategory {
+  _embedded: {
+    productCategory: ProductCategory[];
   }
 }
