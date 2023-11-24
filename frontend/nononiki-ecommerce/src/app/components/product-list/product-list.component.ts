9e7fbc6a07b887d4f77712f81000768b69280055
unknown <nvyas3095@gmail.com>
2023-11-24 01:00:06 -0800

    Enhanced the HTML template to use ng-bootstrap pagination component. Added drop-down list for page size to the HTML template. The list also accounts for maximum page size and getting to the first and last page in one click. Added pagination support to ProductService. Updated ProductListComponent to handle pagination. This included having pagination working for keywords, as well as, categories. Also accounted for searching for products with whitespace in the search bar.

--- frontend/nononiki-ecommerce/src/app/components/product-list/product-list.component.ts
+++ frontend/nononiki-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -17,8 +17,10 @@ export class ProductListComponent implements OnInit {
 
   // Properties for Pagination
   thePageNumber: number = 1;
-  thePageSize: number = 10;
+  thePageSize: number = 5;
   theTotalElements: number = 0;
+
+  previousKeyword: string = "";
 
   constructor(private productService: ProductService,
     private route: ActivatedRoute) { }
@@ -41,14 +43,21 @@ export class ProductListComponent implements OnInit {
   }
 
   handleSearchProducts() {
-    const theKeyword: string = this.route.snapshot.paramMap.get('keyword')!;
+    const theKeyword: string = this.route.snapshot.paramMap.get('keyword')?.trim()!;
+
+    // If we have a different keyword than previous, then set thePageNumber to 1
+    if (this.previousKeyword != theKeyword) {
+      this.thePageNumber = 1;
+    }
+
+    this.previousKeyword = theKeyword;
+    console.log(`keyword=${theKeyword}, thePageNumber = ${this.thePageNumber}`);
 
     // Search for products using keyword
-    this.productService.searchProducts(theKeyword).subscribe(
-      data => {
-        this.products = data;
-      }
-    )
+    this.productService.searchProductsPaginate(
+      this.thePageNumber - 1,
+      this.thePageSize,
+      theKeyword).subscribe(this.processResult());
   }
 
   handleListProducts() {
@@ -79,13 +88,21 @@ export class ProductListComponent implements OnInit {
     this.productService.getProductListPaginate(
       this.thePageNumber - 1,
       this.thePageSize,
-      this.currentCategoryId).subscribe(
-        data => {
-          this.products = data._embedded.products;
-          this.thePageNumber = data.page.number + 1;
-          this.thePageSize = data.page.size;
-          this.theTotalElements = data.page.totalElements;
-        }
-      );
+      this.currentCategoryId).subscribe(this.processResult());
+  }
+
+  updatePageSize(pageSize: string) {
+    this.thePageSize = +pageSize;
+    this.thePageNumber = 1;
+    this.listProducts();
+  }
+
+  processResult() {
+    return (data: any) => {
+      this.products = data._embedded.products;
+      this.thePageNumber = data.page.number + 1;
+      this.thePageSize = data.page.size;
+      this.theTotalElements = data.page.totalElements;
+    }
   }
 }
