b7fb3da8c5d6cab97fff9fd92eb9dafe97a3c731
unknown <nvyas3095@gmail.com>
2023-12-04 14:50:00 -0800

    Modified ProductListComponent to call the CartService. Enhanced the CartStatusComponent to subscribe to the CartService and updated the CartStatusComponent HTML to display cart total, price, and quantity. Learned about the Array.find() method. Added click handler for Add to Cart button on the Product Details page. Fixed an error i was getting due to empty field assignment. Updated ProductDetailsComponent with the click handler button.

--- frontend/nononiki-ecommerce/src/app/common/cart-item.ts
+++ frontend/nononiki-ecommerce/src/app/common/cart-item.ts
@@ -2,10 +2,17 @@ import { Product } from "./product";
 
 export class CartItem {
 
-    constructor(public id: string,
-        public name: string,
-        public imageUrl: string,
-        public unitPrice: number,
-        public quantity: number = 1) {
+    public id: number;
+    public name: string;
+    public imageUrl: string;
+    public unitPrice: number;
+    public quantity: number;
+
+    constructor(product: Product) {
+        this.id = product.id;
+        this.name = product.name;
+        this.imageUrl = product.imageUrl;
+        this.unitPrice = product.unitPrice;
+        this.quantity = 1;
     }
 }
--- frontend/nononiki-ecommerce/src/app/components/product-details/product-details.component.ts
+++ frontend/nononiki-ecommerce/src/app/components/product-details/product-details.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { CartItem } from 'src/app/common/cart-item';
 import { Product } from 'src/app/common/product';
+import { CartService } from 'src/app/services/cart.service';
 import { ProductService } from 'src/app/services/product.service';
 
 @Component({
@@ -10,9 +12,10 @@ import { ProductService } from 'src/app/services/product.service';
 })
 export class ProductDetailsComponent {
 
-  product!: Product;
+  product : Product = {} as Product;
 
   constructor(private productService: ProductService,
+              private cartService: CartService,
               private route: ActivatedRoute) {}
 
   ngOnInit(): void {
@@ -30,5 +33,11 @@ export class ProductDetailsComponent {
         this.product = data;
       }
     )
+  }
+
+  addToCart() {
+    console.log(`Adding to cart: ${this.product.name}, ${this.product.unitPrice}`);
+    const theCartItem = new CartItem(this.product);
+    this.cartService.addToCart(theCartItem);
   }
 }
