feef540105077fb587ff276f30eed13321c9cb49
unknown <nvyas3095@gmail.com>
2023-12-17 21:35:56 -0800

    Specified validation rules for the form controls for the Customer group. Defined getter methods to access form controls and updated HTML template to display error messages for the Customer group. Added event handler to check validation status when the submit button is clicked for Customer group. Updated HTML templates for Last Name and Email to display error messages.

--- frontend/nononiki-ecommerce/src/app/components/checkout/checkout.component.ts
+++ frontend/nononiki-ecommerce/src/app/components/checkout/checkout.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Country } from 'src/app/common/country';
 import { State } from 'src/app/common/state';
 import { NonoNikiShopFormService } from 'src/app/services/nono-niki-shop-form.service';
@@ -28,9 +28,10 @@ export class CheckoutComponent implements OnInit {
   ngOnInit(): void {
     this.checkoutFormGroup = this.formBuilder.group({
       customer: this.formBuilder.group({
-        firstName: [''],
-        lastName: [''],
-        email: ['']
+        firstName: new FormControl('', [Validators.required, Validators.minLength(2)]),
+        lastName: new FormControl('', [Validators.required, Validators.minLength(2)]),
+        email: new FormControl('',
+          [Validators.required, Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')])
       }),
       shippingAddress: this.formBuilder.group({
         street: [''],
@@ -83,6 +84,10 @@ export class CheckoutComponent implements OnInit {
     )
   }
 
+  get firstName() { return this.checkoutFormGroup.get('customer.firstName'); }
+  get lastName() { return this.checkoutFormGroup.get('customer.lastName'); }
+  get email() { return this.checkoutFormGroup.get('customer.email'); }
+
   copyShippingAddressToBillingAddress(event) {
     if (event.target.checked) {
       this.checkoutFormGroup.controls['billingAddress']
@@ -97,6 +102,11 @@ export class CheckoutComponent implements OnInit {
 
   onSubmit() {
     console.log("Handling the submit button.");
+
+    if (this.checkoutFormGroup.invalid) {
+      this.checkoutFormGroup.markAllAsTouched();
+    }
+
     console.log(this.checkoutFormGroup.get('customer').value);
     console.log("The email address is: " + this.checkoutFormGroup.get('customer').value.email);
 
@@ -143,7 +153,7 @@ export class CheckoutComponent implements OnInit {
         else {
           this.billingAddressStates = data;
         }
-        
+
         // Select first item as default
         formGroup.get('state').setValue(data[0]);
       }
