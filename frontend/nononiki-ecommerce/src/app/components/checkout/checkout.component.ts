feafd71cae9828603872a8d4f90123365a315b0b
unknown <nvyas3095@gmail.com>
2024-01-09 13:23:56 -0800

    Updated HTML template to display error messages for Shipping Address.

--- frontend/nononiki-ecommerce/src/app/components/checkout/checkout.component.ts
+++ frontend/nononiki-ecommerce/src/app/components/checkout/checkout.component.ts
@@ -105,6 +105,12 @@ export class CheckoutComponent implements OnInit {
   get lastName() { return this.checkoutFormGroup.get('customer.lastName'); }
   get email() { return this.checkoutFormGroup.get('customer.email'); }
 
+  get shippingAddressStreet() { return this.checkoutFormGroup.get('shippingAddress.street'); }
+  get shippingAddressCity() { return this.checkoutFormGroup.get('shippingAddress.city'); }
+  get shippingAddressState() { return this.checkoutFormGroup.get('shippingAddress.state'); }
+  get shippingAddressCountry() { return this.checkoutFormGroup.get('shippingAddress.country'); }
+  get shippingAddressZipCode() { return this.checkoutFormGroup.get('shippingAddress.zipCode'); }
+
   copyShippingAddressToBillingAddress(event) {
     if (event.target.checked) {
       this.checkoutFormGroup.controls['billingAddress']
